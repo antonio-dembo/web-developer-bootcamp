544f8d99abd6ba3dcd3bd8f607ff528d332f81ba
beboantonio <beboantonio1@hotmail.com>
2021-09-19 06:13:17 -0300

    add JS literals

---
+++ Javasript/JSObjectLiterals/app.js
@@ -0,0 +1,24 @@
+const person = {
+    firstame : "Antonio",
+    lastname : "Dembo"
+}
+
+const years = { 1999 : 'GOOD', 2020 : 'BAD'}
+
+// Exercise
+const restaurant = {
+    name: 'Ichiran Ramen',
+    address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
+    city: 'Brooklyn',
+    state: 'NY',
+    zipcode: '11206',
+}
+
+//YOUR CODE GOES DOWN HERE:
+const fullAddress = `${restaurant.address}, ${restaurant.city}, ${restaurant.state} ${restaurant.zipcode}`
+
+// ARRAYS + OBJECTS
+const comments = [
+    { username: 'Tammy', text: 'lolololol', votes: 9},
+    { username: 'FishBoi', text: 'glub glub glub', votes: 12387}
+]
