1473d4e0695d0528c204677966e490dd359ce4e7
beboantonio <beboantonio1@hotmail.com>
2021-09-19 06:54:40 -0300

    solve the forest exercise

---
+++ Javasript/IntroToDOM/magical-forest-circle-exercise.js
@@ -0,0 +1,12 @@
+// use Javascript to make the following changes:
+
+// select div with the id of 'container'. using Javascript
+// set it's text alignment to 'center'
+const content = document.querySelector('#content');
+content.align = 'center';
+
+// select the image and use Javscript to git it a width of '150px' and 
+// a border radious of '50%'
+const img = document.querySelector('img');
+img.style.width = '150px';
+img.style.borderRadius = '50%';
